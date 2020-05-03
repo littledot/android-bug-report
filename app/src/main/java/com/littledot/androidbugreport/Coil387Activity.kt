2e47fee255b26150a26dff729a3d32ca557a5601
littledot5566 <littledot5566@gmail.com>
2020-05-03 18:31:46 -0400

    reported

--- app/src/main/java/com/littledot/androidbugreport/Coil387Activity.kt
+++ app/src/main/java/com/littledot/androidbugreport/Coil387Activity.kt
@@ -5,13 +5,16 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import coil.Coil
 import coil.api.loadAny
-import kotlinx.android.synthetic.main.main.*
+import kotlinx.android.synthetic.main.coil_387.*
 
-class MainActivity : AppCompatActivity() {
+/**
+ * https://github.com/coil-kt/coil/issues/387
+ */
+class Coil387Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
+        setContentView(R.layout.coil_387)
 
         val src =
             "https://pbs.twimg.com/profile_images/378800000666093975/cbe970ce9a19d2c6bec0d06c6004ccf0_400x400.png"
